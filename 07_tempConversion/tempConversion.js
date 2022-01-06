4cb590f7ae691557f0f800370174e3a6f9f64c69
Saaransh Sharma <saaranshsharma28@gmail.com>
2022-01-06 14:42:47 -0500

    6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const convert = (fahrenheit-32)*(5/9);
+  return Math.round(convert*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  const convert = (celsius*(9/5))+32;
+  return Math.round(convert*10)/10;
 };
 
 // Do not edit below this line
